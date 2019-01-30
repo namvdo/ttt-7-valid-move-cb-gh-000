1ac465e2b20bf47616883120af457b1e5894a7c2
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 07:53:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,5 +3,26 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move?(board, index)
+  def position_taken?(array, index)
+    if array[index] == "" || array[index] = " " || array[index] = nil
+      return false
+    else
+      return true
+    end
+  end
+  def onboardornot?(number)
+    if number.between(0,8) == true
+      return true
+    else
+      return false
+    end
+  end
+if(position_taken?(board,index) == false && onboardornot?(number) == true)
+  return true
+else
+  return false
+end
+end 
+    
 
 end
