79f0c37b5a2c6e34b93b391e9dbf4e3d88b33efc
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 08:56:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,34 @@
+# code your #valid_move? method here
+
+require_relative '../spec/valid_move_spec.rb'
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == "" || array[ind] = " " || array[ind] = nil
+      return false
+    else
+      return true
+    end
+  end
+  
+  def onboardornot?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+if position_taken?(board, index) == false && onboardornot?(index) == true
+  return true
+else
+  return false
+end
+
+end
+
+
+
+-------
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
@@ -23,3 +54,4 @@ def valid_move?(board, index)
 
 
 end
+
