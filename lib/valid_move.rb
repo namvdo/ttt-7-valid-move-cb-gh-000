e5feaf4c5e8a32e6eb6306fdee13e94790aed4a4
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 07:30:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,9 @@
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move(move)
   board = ["","","","","","","","",""]
+  for i=0; i<board.length;i++
+    index = board[i]
+  end 
   if position_taken board[index] == "" || board[index] == " " || board[index] == nil
            false
         else
