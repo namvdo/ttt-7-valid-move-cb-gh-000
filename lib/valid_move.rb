f21492b86a6f5fa14324a822d5f0cfa5ff5f666e
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 07:35:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,8 +3,7 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move(move)
-  input_to_index()
-  gets input
+  gets 
   index = input.to_i - 1
 end 
   if position_taken board[index] == "" || board[index] == " " || board[index] == nil
