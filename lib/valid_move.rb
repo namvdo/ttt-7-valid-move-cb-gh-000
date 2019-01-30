9dfbef84a300d6c791bc411449f87613d946c3bc
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 08:05:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -19,6 +19,8 @@ def valid_move?(board, index)
   end
 if position_taken?(board, index) == false && onboardornot?(index) == true
   return true
+elsif position_taken?(board, index) == true && onboardornot?(index) == true
+  return false
 else
   return false
 end
